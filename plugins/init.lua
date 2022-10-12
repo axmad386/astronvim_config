3ac376726005cc3b274dc9bc3914a7c966bbad3f
Akhmad Salafudin <axmad386@gmail.com>
2022-10-12 11:03:32 +0700

    feat(options): add default vim options

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,4 +1,8 @@
 return {
+  -- treesitter = { -- overrides `require("treesitter").setup(...)`
+  --   ensure_installed = { "lua" },
+  --   indent = { enable = true },
+  -- },
   ["tpope/vim-fugitive"] = {},
   ["Mohammed-Taher/AdvancedNewFile.nvim"] = {},
   ["folke/tokyonight.nvim"] = {},
