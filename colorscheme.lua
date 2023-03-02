15e3952492afb982d975c24cee610573b53aa605
Akhmad Salafudin <axmad386@gmail.com>
2023-03-02 22:00:35 +0700

    feat(colorscheme): remove transparency

--- colorscheme.lua
+++ colorscheme.lua
@@ -1,7 +1,8 @@
 require("tokyonight").setup {
     style = "night", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
-    transparent = true, -- Enable this to disable setting the background color
+    transparent = false, -- Enable this to disable setting the background color
     terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim                                                                                                  |
+
     styles = {
         -- Style to be applied to different syntax groups
         -- Value is any valid attr-list value `:help attr-list`
@@ -10,14 +11,14 @@ require("tokyonight").setup {
         functions = "NONE",
         variables = "italic",
         -- Background styles. Can be "dark", "transparent" or "normal"
-        sidebars = "transparent", -- style for sidebars, see below
-        floats = "transparent", -- style for floating windows
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
     },
     sidebars = { "qf", "help", "vista_kind", "terminal", "packer" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`                                                      |
     day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors                                                  |
-    hide_inactive_statusline = false, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**. |
-    dim_inactive = false, -- dims inactive windows
-    lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold                                                                                                  |
+    hide_inactive_statusline = true, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**. |
+    dim_inactive = true, -- dims inactive windows
+    lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold                                                                                                  |
 }
 
 return "tokyonight"
