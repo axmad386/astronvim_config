4763a600b5ebeda6c298f6ab341dd35badb8cd96
Akhmad Salafudin <axmad386@gmail.com>
2023-03-24 10:06:25 +0700

    feat(plugin): quickfixdd for deleting list in quicfix with dd

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,9 +1,22 @@
 return {
+  -- buat GIT
   { "tpope/vim-fugitive",     cmd = "G" },
+
   { "fatih/vim-go" },
   { "jwalton512/vim-blade" },
+
+  -- Semacam BetterPest kalau di VsCode
   { "vim-test/vim-test",      cmd = "Test" },
+
+  -- untuk replace kata otomatis sesuai casenya
   { "tpope/vim-abolish",      cmd = { "S", "Subvert" } },
+
+  -- untuk search word di banyak file atau folder
   { 'mileszs/ack.vim',        cmd = "Ack" },
-  { 'schickling/vim-bufonly', cmd = "Bufonly" }
+
+  -- untuk close all buffer kecuali current buffer
+  { 'schickling/vim-bufonly', cmd = "Bufonly" },
+
+  -- untuk delete list di quicklist dengan dd
+  { 'TamaMcGlinn/quickfixdd', cmd = "Ack" },
 }
